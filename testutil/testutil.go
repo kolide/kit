e95971f42a4f3098a1267822fbe5d66f5b747069
Zachary Wasserman <zachwass2000@gmail.com>
2017-08-01 10:57:38 -0400

    Remove async on FatalAfter

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -41,24 +41,16 @@ func ErrorAfterFunc(timeout time.Duration, f func()) error {
 // FatalAfterFunc will fatal the test after the provided timeout has passed if
 // the provided function has not returned.
 func FatalAfterFunc(t testing.TB, timeout time.Duration, f func()) {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		f()
-	}()
-	if err := ErrorAfter(timeout, &wg); err != nil {
+	if err := ErrorAfterFunc(timeout, f); err != nil {
 		t.Fatal(err)
 	}
 }
 
-// FatalAfter will (asynchronously) fatal the test after the provided timeout
-// has passed if the provided WaitGroup has not unblocked. All Add() calls to
-// the WaitGroup must be performed before calling this function.
+// FatalAfter will fatal the test after the provided timeout has passed if the
+// provided WaitGroup has not unblocked. All Add() calls to the WaitGroup must
+// be performed before calling this function.
 func FatalAfter(t testing.TB, timeout time.Duration, wg *sync.WaitGroup) {
-	go func() {
-		if err := ErrorAfter(timeout, wg); err != nil {
-			t.Fatal(err)
-		}
-	}()
+	if err := ErrorAfter(timeout, wg); err != nil {
+		t.Fatal(err)
+	}
 }
