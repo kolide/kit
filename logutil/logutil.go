97f3b7fee57d06e7715aa193a77fbde9957b38a7
Mike Arpaia <mike@arpaia.co>
2018-03-16 10:54:21 -0600

    refactor shared implementation into helper

--- logutil/logutil.go
+++ logutil/logutil.go
@@ -44,37 +44,7 @@ func SetLevelKey(logger log.Logger, key interface{}) log.Logger {
 // The acceptable level can be swapped by sending SIGUSR2 to the process.
 func NewServerLogger(debug bool) log.Logger {
 	base := log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
-	base = log.With(base, "ts", log.DefaultTimestampUTC)
-	base = SetLevelKey(base, "severity")
-	base = level.NewInjector(base, level.InfoValue())
-
-	lev := level.AllowInfo()
-	if debug {
-		lev = level.AllowDebug()
-	}
-
-	base = log.With(base, "caller", log.Caller(6))
-
-	var swapLogger log.SwapLogger
-	swapLogger.Swap(level.NewFilter(base, lev))
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGUSR2)
-	go func() {
-		for {
-			<-sigChan
-			if debug {
-				newLogger := level.NewFilter(base, level.AllowInfo())
-				swapLogger.Swap(newLogger)
-			} else {
-				newLogger := level.NewFilter(base, level.AllowDebug())
-				swapLogger.Swap(newLogger)
-			}
-			level.Info(&swapLogger).Log("msg", "swapping level", "debug", !debug)
-			debug = !debug
-		}
-	}()
-	return &swapLogger
+	return newLogger(debug, base)
 }
 
 // NewCLILogger creates a standard logger for Kolide CLI tools.
@@ -83,6 +53,10 @@ func NewServerLogger(debug bool) log.Logger {
 // The acceptable level can be swapped by sending SIGUSR2 to the process.
 func NewCLILogger(debug bool) log.Logger {
 	base := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	return newLogger(debug, base)
+}
+
+func newLogger(debug bool, base log.Logger) log.Logger {
 	base = log.With(base, "ts", log.DefaultTimestampUTC)
 	base = SetLevelKey(base, "severity")
 	base = level.NewInjector(base, level.InfoValue())
