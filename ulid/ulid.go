d4e9bf8ecdcfb465f0abb06658b3317725ef20fe
Mike Arpaia <mike@arpaia.co>
2018-08-03 11:16:36 -0600

    Fix typo in GoDoc

--- ulid/ulid.go
+++ ulid/ulid.go
@@ -6,7 +6,7 @@ import (
 	"github.com/oklog/ulid"
 )
 
-// New returns a Universally Unique Lexicographically Sortable Identifier viia
+// New returns a Universally Unique Lexicographically Sortable Identifier via
 // github.com/oklog/ulid
 func New() string {
 	return ulid.MustNew(ulid.Now(), rand.Reader).String()
