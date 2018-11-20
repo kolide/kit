4bca038f31aa15a011b8cf27b1db9b7b3d242626
Victor Vrantchan <vrancean+github@gmail.com>
2018-11-20 11:28:11 -0500

    Add options to wrap driver with OpenCensus SQL.

--- dbutil/dbutil.go
+++ dbutil/dbutil.go
@@ -9,12 +9,16 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/jmoiron/sqlx"
+	"github.com/opencensus-integrations/ocsql"
 	"github.com/pkg/errors"
 )
 
 type dbConfig struct {
-	logger      log.Logger
-	maxAttempts int
+	logger                  log.Logger
+	maxAttempts             int
+	wrapWithCensus          bool
+	alreadyRegisteredDriver bool
+	censusTraceOptions      []ocsql.TraceOption
 }
 
 // WithLogger configures a logger Option.
@@ -31,6 +35,26 @@ func WithMaxAttempts(maxAttempts int) Option {
 	}
 }
 
+func WithCensusDriver() Option {
+	return func(c *dbConfig) {
+		c.wrapWithCensus = true
+	}
+}
+
+func WithCensusTraceOptions(opts ...ocsql.TraceOption) Option {
+	return func(c *dbConfig) {
+		c.censusTraceOptions = opts
+	}
+}
+
+// alreadyRegisteredDriver is a private option to use when passing options from
+// OpenDBX to OpenDB.
+func alreadyRegisteredDriver() Option {
+	return func(c *dbConfig) {
+		c.alreadyRegisteredDriver = true
+	}
+}
+
 // Option provides optional configuration for managing DB connections.
 type Option func(*dbConfig)
 
@@ -40,12 +64,21 @@ type Option func(*dbConfig)
 // the maxAttempts value(defaults to 15 attempts).
 func OpenDB(driver, dsn string, opts ...Option) (*sql.DB, error) {
 	config := &dbConfig{
-		logger:      log.NewNopLogger(),
-		maxAttempts: 15,
+		logger:             log.NewNopLogger(),
+		censusTraceOptions: []ocsql.TraceOption{ocsql.WithAllTraceOptions()},
+		maxAttempts:        15,
 	}
 
 	for _, opt := range opts {
 		opt(config)
+	}
+
+	if config.wrapWithCensus && !config.alreadyRegisteredDriver {
+		driverName, err := ocsql.Register(driver, config.censusTraceOptions...)
+		if err != nil {
+			return nil, errors.Wrapf(err, "wrapping driver %s with opencensus sql %s", driver, driverName)
+		}
+		driver = driverName
 	}
 
 	db, err := sql.Open(driver, dsn)
@@ -75,10 +108,28 @@ func OpenDB(driver, dsn string, opts ...Option) (*sql.DB, error) {
 // dbutil.OpenDBX is similar to dbutil.OpenDB, except it returns a *sqlx.DB from
 // the popular github.com/jmoiron/sqlx package.
 func OpenDBX(driver, dsn string, opts ...Option) (*sqlx.DB, error) {
-	db, err := OpenDB(driver, dsn, opts...)
+	config := &dbConfig{
+		censusTraceOptions: []ocsql.TraceOption{ocsql.WithAllTraceOptions()},
+	}
+	for _, opt := range opts {
+		opt(config)
+	}
+
+	driverName := driver
+	if config.wrapWithCensus && !config.alreadyRegisteredDriver {
+		driverName, err := ocsql.Register(driver, config.censusTraceOptions...)
+		if err != nil {
+			return nil, errors.Wrapf(err, "wrapping driver %s with opencensus sql %s", driver, driverName)
+		}
+		opts = append(opts, alreadyRegisteredDriver())
+		driverName = driverName
+	}
+
+	db, err := OpenDB(driverName, dsn, opts...)
 	if err != nil {
 		return nil, errors.Wrap(err, "opening database/sql database connection")
 	}
 
+	// never wrap the NewDb with a driver name, just sql.Open
 	return sqlx.NewDb(db, driver), nil
 }
