c25407cb8bb44036de3b4f32d1c33771e3e4f65f
seph <seph@directionless.org>
2020-08-12 01:34:44 -0400

    lint

--- stringutil/stringutil.go
+++ stringutil/stringutil.go
@@ -4,12 +4,13 @@ import (
 	"math/rand"
 )
 
-// RandomString returns a 'random' string. Don't rely on this to have a secure
-// level of entropy.
+// RandomString returns a 'random' string. Don't rely on this to have
+// a secure level of entropy.
 func RandomString(n int) string {
 	letterBytes := "abcdefghijklmnopqrstuvwxyz"
 	b := make([]byte, n)
 	for i := range b {
+		/* #nosec */
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return string(b)
