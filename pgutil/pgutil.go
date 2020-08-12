40c6a6facc6c25fff6b4c6de56df9839bb92fd01
seph <seph@directionless.org>
2020-08-12 01:11:20 -0400

    pgutil options
    Support for sslmodes, and url parsing

--- pgutil/pgutil.go
+++ pgutil/pgutil.go
@@ -3,6 +3,10 @@ package pgutil
 
 import (
 	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
 // ConnectionOptions represents the configurable options of a connection to a
@@ -14,6 +18,71 @@ type ConnectionOptions struct {
 	Password string
 	DBName   string
 	SSLMode  string
+}
+
+type Opts func(*ConnectionOptions)
+
+// Supported SSL modes.
+type sslMode string
+
+const (
+	SSLBlank      sslMode = ""
+	SSLDisable            = "disable"
+	SSLAllow              = "allow"
+	SSLPrefer             = "prefer"
+	SSLRequire            = "require"
+	SSLVerifyCa           = "verify-ca"
+	SSLVerifyFull         = "verify-full"
+)
+
+// WithSSL sets the sslmode parameter for postgresql. See the
+// postgresql documentation at
+// https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+func WithSSL(requestedSslMode sslMode) Opts {
+	return func(c *ConnectionOptions) {
+		c.SSLMode = string(requestedSslMode)
+	}
+}
+
+// NewFromURL returns a ConnectionOptions from a given URL. This uses
+// a format like `postgres://myuser:mypass@localhost/somedatabase`,
+// which is commonly found on hosting platforms.
+func NewFromURL(rawurl string, opts ...Opts) (ConnectionOptions, error) {
+	var c ConnectionOptions
+	parsed, err := url.Parse(rawurl)
+	if err != nil {
+		return c, err
+	}
+
+	c = ConnectionOptions{
+		Host:    parsed.Host,
+		User:    parsed.User.Username(),
+		DBName:  strings.TrimPrefix(parsed.Path, "/"),
+		SSLMode: string(SSLRequire),
+	}
+
+	if pass, ok := parsed.User.Password(); ok {
+		c.Password = pass
+	}
+
+	// Split the URL host/port into parts
+	hostComponents := strings.Split(parsed.Host, ":")
+	switch len(hostComponents) {
+	case 1:
+		c.Host = hostComponents[0]
+		c.Port = "5432"
+	case 2:
+		c.Host = hostComponents[0]
+		c.Port = hostComponents[1]
+	default:
+		return c, errors.Errorf("Could not parse %s as host:port", parsed.Host)
+	}
+
+	for _, opt := range opts {
+		opt(&c)
+	}
+
+	return c, nil
 }
 
 // String implements the Stringer interface so that a pgutil.ConnectionOptions
---
+++ pgutil/pgutil_test.go
@@ -0,0 +1,51 @@
+package pgutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestConversion(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		in   string
+		opts []Opts
+		out  string
+		err  bool
+	}{
+		{
+			in:  "postgres://myuser:mypass@localhost/somedatabase",
+			out: "host=localhost port=5432 dbname=somedatabase sslmode=require user=myuser password=mypass",
+		},
+		{
+			in:  "postgres://myuser@localhost:1234/somedatabase",
+			out: "host=localhost port=1234 dbname=somedatabase sslmode=require user=myuser",
+		},
+		{
+			in:   "postgres://myuser:mypass@localhost/somedatabase",
+			opts: []Opts{WithSSL(SSLBlank)},
+			out:  "host=localhost port=5432 dbname=somedatabase sslmode= user=myuser password=mypass",
+		},
+		{
+			in:   "postgres://myuser:mypass@localhost/somedatabase",
+			opts: []Opts{WithSSL(SSLDisable)},
+			out:  "host=localhost port=5432 dbname=somedatabase sslmode=disable user=myuser password=mypass",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			c, err := NewFromURL(tt.in, tt.opts...)
+			if tt.err {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tt.out, c.String())
+		})
+	}
+
+}
