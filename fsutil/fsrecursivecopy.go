17eca5d2e6d26f2bdd102e281b0b89d65a614685
seph <seph@kolide.co>
2022-09-20 17:28:10 -0400

    Add file permissions to `CopyFSToDisk` util (#53)

--- fsutil/fsrecursivecopy.go
+++ fsutil/fsrecursivecopy.go
@@ -7,19 +7,29 @@ import (
 	"path"
 )
 
-func CopyFSToDisk(src fs.FS, destDir string) error {
-	if err := fs.WalkDir(src, ".", genCopyToDiskFunc(src, destDir)); err != nil {
+// CopyFSToDisk copies an embedded FS to a given directory. Because go's embed does not preserve the file mode, you must
+// also pass a function that will return the desired file mode for each file.
+func CopyFSToDisk(src fs.FS, destDir string, modeSetter func(fs.FileInfo) os.FileMode) error {
+	if err := fs.WalkDir(src, ".", genCopyToDiskFunc(src, destDir, modeSetter)); err != nil {
 		return fmt.Errorf("walking directory: %w", err)
 	}
 
 	return nil
+}
 
+// CommonFileMode is a function that returns the common file permissions: 0644 for all files, and 0755 for
+// all directories. It is provided as a helper for CopyFSToDisk's common use case
+func CommonFileMode(fi fs.FileInfo) fs.FileMode {
+	if fi.IsDir() {
+		return 0755
+	} else {
+		return 0644
+	}
 }
 
 // genCopyToDiskFunc returns fs.WalkDirFunc function that will
 // copy files to disk in a given location.
-func genCopyToDiskFunc(srcFS fs.FS, destDir string) fs.WalkDirFunc {
-
+func genCopyToDiskFunc(srcFS fs.FS, destDir string, modeSetter func(fs.FileInfo) os.FileMode) fs.WalkDirFunc {
 	return func(filepath string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -34,7 +44,7 @@ func genCopyToDiskFunc(srcFS fs.FS, destDir string) fs.WalkDirFunc {
 
 		// If it's a directory, make it under destdir
 		if d.IsDir() {
-			if err := os.MkdirAll(fullpath, fileinfo.Mode()); err != nil {
+			if err := os.MkdirAll(fullpath, modeSetter(fileinfo)); err != nil {
 				return fmt.Errorf("making directory %s: %w", fullpath, err)
 			}
 			return nil
@@ -45,7 +55,7 @@ func genCopyToDiskFunc(srcFS fs.FS, destDir string) fs.WalkDirFunc {
 			return fmt.Errorf("reading file from FS %s: %w", filepath, err)
 		}
 
-		if err := os.WriteFile(fullpath, data, fileinfo.Mode()); err != nil {
+		if err := os.WriteFile(fullpath, data, modeSetter(fileinfo)); err != nil {
 			return fmt.Errorf("writing %s: %w", filepath, err)
 		}
 
