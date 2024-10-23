697224b754635404aef7c333fe55880750aad88c
zack olson <zack@kolide.co>
2024-10-23 11:26:33 -0400

    add version number generation logic and tests

--- version/version.go
+++ version/version.go
@@ -24,7 +24,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"regexp"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 // These values are private which ensures they can only be set with the build flags.
@@ -47,6 +50,10 @@ type Info struct {
 	BuildDate string `json:"build_date"`
 	BuildUser string `json:"build_user"`
 }
+
+// semverRegexp is used to standardize various version strings by pulling out only
+// the major.minor.patch submatch
+var semverRegexp = regexp.MustCompile(`^v?([0-9]+\.[0-9]+\.[0-9]+).*$`)
 
 // Version returns a structure with the current version information.
 func Version() Info {
@@ -86,4 +93,68 @@ func Handler() http.Handler {
 		enc.SetIndent("", "  ")
 		enc.Encode(v)
 	})
+}
+
+// versionMultiplier consts are used to create a comparable and reversible integer version from a semver string
+// by using 1 million, 1 thousand, and 1 for each part we avoid collisions as long as all parts are less than 1 thousand
+const (
+	majorVersionMultiplier int = 1000000
+	minorVersionMultiplier int = 1000
+	patchVersionMultiplier int = 1
+)
+
+// VersionNum parses the semver version string to look for only the major.minor.patch portion,
+// splits that into 3 parts (disregarding any extra portions), and applies a multiplier to each
+// part to generate a total int value representing the semver.
+// note that this will generate a sortable, and reversible integer as long as all parts remain less than 1000
+// This is currently intended for use in generating comparable versions to set within windows registry entries,
+// allowing for an easy "upgrade-only" detection configuration within intune.
+// Zero is returned for any case where the version cannot be reliably translated
+func VersionNum() int {
+	semverMatch := semverRegexp.FindStringSubmatch(version)
+	// expect the leftmost match as semverMatch[0] and the semver substring as semverMatch[1]
+	if semverMatch == nil || len(semverMatch) != 2 {
+		return 0
+	}
+
+	parts := strings.Split(semverMatch[1], ".")
+	if len(parts) < 3 {
+		return 0
+	}
+
+	versionNum := 0
+	for i, part := range parts[:3] {
+		partNum, err := strconv.Atoi(part)
+		if err != nil {
+			return 0
+		}
+
+		switch i {
+		case 0:
+			versionNum += (partNum * majorVersionMultiplier)
+		case 1:
+			versionNum += (partNum * minorVersionMultiplier)
+		case 2:
+			versionNum += (partNum * patchVersionMultiplier)
+		}
+	}
+
+	return versionNum
+}
+
+// SemverFromVersionNum provides the inverse functionality of VersionNum, allowing us
+// to collect and report the integer version in a readable semver format
+func SemverFromVersionNum(versionNum int) string {
+	if versionNum == 0 {
+		return "0.0.0"
+	}
+
+	major := versionNum / majorVersionMultiplier
+	remaining := versionNum - (major * majorVersionMultiplier)
+	minor := remaining / minorVersionMultiplier
+	remaining = remaining - (minor * minorVersionMultiplier)
+	// not strictly needed because patchVersionMultiplier is 1 but here because it feels correct
+	patch := remaining * patchVersionMultiplier
+
+	return fmt.Sprintf("%d.%d.%d", major, minor, patch)
 }
